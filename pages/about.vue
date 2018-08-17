b4d2d44ef809d9fe7de944bb5b0b722a5299b2f6
SirawichDev <sirawit0676@gmail.com>
2018-08-17 11:34:30 +0700

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Miewmiews</h1>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    h1 {
+        font-size: 20px;
+        font-weight: 800;
+    }
+</style>
