6ac064da90461b7726bd7b5124cfb82a639f90a5
SirawichDev <sirawit0676@gmail.com>
2018-08-17 14:08:51 +0700

    make route param

---
+++ pages/user/_id/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="card">
+        <div class="card-image">
+        <h1>Hello User Id : {{$route.params.id}}</h1>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+
+</style>
