f13b99e8ce606a24710f62e4c30b9568488faaef
SirawichDev <sirawit0676@gmail.com>
2018-08-17 17:07:01 +0700

    error.vue will occured when have an error with route

---
+++ layouts/error.vue
@@ -0,0 +1,10 @@
+<template>
+    <article class="message is-danger">
+  <div class="message-header">
+    <p>Error</p>
+  </div>
+  <div class="message-body">
+      Sorry We can find only ID 💔
+  </div>
+</article>
+</template>
